1fe3ca3694185d4ccd6750837ff7a5d1c2f3b254
Promise <976949689@qq.com>
2021-07-03 18:02:37 +0800

    2021年7月3日18:02:32

--- 6.SpringMVC_viewResolver/src/com/clearlove/view/MyView.java
+++ 6.SpringMVC_viewResolver/src/com/clearlove/view/MyView.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.View;
 
 /**
  * 自定义视图
+ *
  * @author promise
  * @date 2021/7/3 - 16:42
  */
@@ -24,20 +24,26 @@ public class MyView implements View {
   }
 
   @Override
-  public void render(Map<String, ?> map, HttpServletRequest httpServletRequest,
-      HttpServletResponse httpServletResponse) throws Exception {
+  public void render(
+      Map<String, ?> map,
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse)
+      throws Exception {
     System.out.println("之前保存的数据：" + map);
     httpServletResponse.setContentType("text/html");
     List<String> vn = (List<String>) map.get("video");
     httpServletResponse.getWriter().write("哈哈<h1>即将展现精彩内容</h1>");
     for (String s : vn) {
-      httpServletResponse.getWriter().write("<a>下载" + s +".avi</a><br>");
+      httpServletResponse.getWriter().write("<a>下载" + s + ".avi</a><br>");
     }
-    httpServletResponse.getWriter().write("<script>"
-        + "var aEle = document.getElementsByTagName('a')[0];"
-        + "aEle.onclick = function(){"
-        + "alert('想下载吗?交学费')"
-        + "}"
-        + "</script>");
+    httpServletResponse
+        .getWriter()
+        .write(
+            "<script>"
+                + "var aEle = document.getElementsByTagName('a')[0];"
+                + "aEle.onclick = function(){"
+                + "alert('想下载吗?交学费')"
+                + "}"
+                + "</script>");
   }
 }
