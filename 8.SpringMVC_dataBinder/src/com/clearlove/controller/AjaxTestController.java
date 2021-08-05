011b08f2c84a6f1747a12970a0ffc93d1a3d54c7
Promise <976949689@qq.com>
2021-08-05 23:21:43 +0800

    2021年8月5日23:21:37

--- 8.SpringMVC_dataBinder/src/com/clearlove/controller/AjaxTestController.java
+++ 8.SpringMVC_dataBinder/src/com/clearlove/controller/AjaxTestController.java
@@ -4,6 +4,7 @@ import com.clearlove.bean.Employee;
 import com.clearlove.dao.EmployeeDao;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,12 +37,39 @@ public class AjaxTestController {
   /**
    * @RequestBody ：请求体；获取一个请求的请求体
    * @RequestParam :
+   *
+   * @ResponseBody : 可以把对象转为Json数据，返回给浏览器
+   * @RequestBody ：接收json数据，封装为对象
+   *
    * @return
    */
   @RequestMapping(value = "/testRequestBody")
-  public String testRequestBody(@RequestBody String body) {
-    System.out.println("请求体：" + body);
+  public String testRequestBody(@RequestBody Employee employee) {
+    System.out.println("请求体：" + employee);
     return "success";
   }
+
+
+  @RequestMapping(value = "/test01")
+  public String test01(@RequestBody String str) {
+    System.out.println("请求体：" + str);
+    return "success";
+  }
+
+
+  /**
+   * 如果参数位置写HttpEntity<String> str
+   * 比@RequestBody更强，可以拿到请求头
+   * @RequestHeader ：只能拿到某个请求头
+   *
+   * @param str
+   * @return
+   */
+  @RequestMapping(value = "/test02")
+  public String test02(HttpEntity<String> str) {
+    System.out.println(str);
+    return "success";
+  }
+
 
 }
--- 8.SpringMVC_dataBinder/web/testOther.jsp
+++ 8.SpringMVC_dataBinder/web/testOther.jsp
@@ -13,12 +13,33 @@
         pageContext.setAttribute("ctp", request.getContextPath());
     %>
 </head>
+<script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
 <body>
-<form method="post" action="${ctp}/testRequestBody" enctype="multipart/form-data">
+<%--<form method="post" action="${ctp}/testRequestBody" enctype="multipart/form-data">--%>
+<form method="post" action="${ctp}/test02" enctype="multipart/form-data">
     <input name="username" value="tomcat"/>
     <input name="password" value="123456"/>
     <input type="file" name="file"/>
     <input type="submit"/>
 </form>
+<a href="testRequestBody">ajax发送json数据</a>
 </body>
+<script type="text/javascript">
+    ${"a:first"}.click(function (){
+      // 点击发送ajax请求，请求带的数据是json
+      // js对象
+      var emp = {lastName: "张三", email: "aaa@aa.com", gender: 0};
+      var empStr = JSON.stringify(emp);
+      $.ajax({
+        url:'${ctp}/testRequestBody',
+        type:'POST',
+        contentType: "application/json",
+        data: empStr,
+        success: function (data) {
+          alert(data);
+        }
+      });
+      return false;
+    });
+</script>
 </html>
